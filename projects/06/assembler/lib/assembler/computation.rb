9aaa927476782b34862758732a8d2ff8113b0166
Michael Ries <michael@riesd.com>
2014-05-16 23:25:59 -0600

    computation specs failing

--- projects/06/assembler/lib/assembler/computation.rb
+++ projects/06/assembler/lib/assembler/computation.rb
@@ -1,7 +1,73 @@
 module Assembler
   class Computation < Command
-    def initialize(str)
+    OPERATIONS = {
+      "0"   => "0101010",
+      "1"   => "0111111",
+      "-1"  => "0111010",
+      "D"   => "0001100",
+      "A"   => "0110000",
+      "!D"  => "0001101",
+      "!A"  => "0110001",
+      "-D"  => "0001111",
+      "-A"  => "0110011",
+      "D+1" => "0011111",
+      "A+1" => "0110111",
+      "D-1" => "0001110",
+      "A-1" => "0110010",
+      "D+A" => "0000010",
+      "D-A" => "0010011",
+      "A-D" => "0000111",
+      "D&A" => "0000000",
+      "D|A" => "0010101",
+      "M"   => "1110000",
+      "!M"  => "1110001",
+      "-M"  => "1110011",
+      "M+1" => "1110111",
+      "M-1" => "1110010",
+      "D+M" => "1000010",
+      "D-M" => "1010011",
+      "M-D" => "1000111",
+      "D&M" => "1000000",
+      "D|M" => "1010101",
+    }.freeze
 
+    JUMPS = {
+      "JGT" => "001",
+      "JEQ" => "010",
+      "JGE" => "011",
+      "JLT" => "100",
+      "JNE" => "101",
+      "JLE" => "110",
+      "JMP" => "111",
+    }.freeze
+
+    attr_reader :operation, :destination, :jump
+
+    def initialize(str)
+      if str.include?("=")
+        @destination, str = str.split("=")
+      else
+        @destination = ""
+      end
+      @operation, @jump = str.split(";")
+    end
+
+    def binary
+      "111"+c+d+j
+    end
+
+    def c
+      OPERATIONS.fetch(operation)
+    end
+
+    def d
+      (destination.include?("A") ? "1" : "0") +
+        (destination.include?("D") ? "1" : "0") +
+        (destination.include?("M") ? "1" : "0")
+    end
+
+    def j
+      JUMPS.fetch(jump){ "000" }
     end
   end
 end
---
+++ projects/06/assembler/spec/lib/assembler/computation_spec.rb
@@ -0,0 +1,64 @@
+require 'spec_helper'
+require 'assembler/command'
+
+describe Assembler::Computation do
+  Given(:computation){ Assembler::Computation.new(str) }
+
+  context "parse M=D;JNE" do
+    Given(:str){ "M=D;JNE" }
+    Then{ computation.operation.should == "D" }
+    Then{ computation.destination.should == "M" }
+    Then{ computation.jump.should == "JNE" }
+  end
+
+  context "parse AM=D+1;JGT" do
+    Given(:str){ "AM=D+1;JGT" }
+    Then{ computation.operation.should == "D+1" }
+    Then{ computation.destination.should == "AM" }
+    Then{ computation.jump.should == "JGT" }
+  end
+
+  context "parse 1;JLT" do
+    Given(:str){ "1;JLT" }
+    Then{ computation.operation.should == "1" }
+    Then{ computation.destination.should == "" }
+    Then{ computation.jump.should == "JLT" }
+  end
+
+  context "parse M=D" do
+    Given(:str){ "M=D" }
+    Then{ computation.operation.should == "D" }
+    Then{ computation.destination.should == "M" }
+    Then{ computation.jump.should be_nil }
+  end
+
+  context "#binary" do
+    context "operation only" do
+      {
+        "0"   => "1110101010000000",
+        "1"   => "1110111110000000",
+        "-1"  => "1110111010000000",
+        "D&A" => "1110000000000000",
+      }.each do |command, binary|
+        let(:str){ command }
+        it "it parses #{command}" do
+          computation.binary.should eq(binary)
+        end
+      end
+    end
+
+    context "full commands" do
+      {
+        "D=A"   => "1110110000010000",
+        "D=D+A" => "1110000010010000",
+        "M=D"   => "1110001100001000",
+        "D;JGE" => "1110001100000011",
+      }.each do |command, binary|
+        let(:str){ command }
+        it "it parses #{command}" do
+          computation.binary.should eq(binary)
+        end
+      end
+    end
+  end
+end
