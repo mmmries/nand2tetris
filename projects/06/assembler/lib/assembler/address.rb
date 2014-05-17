ac29b7b9359b6170491da8b1d286500fbb18a60e
Michael Ries <michael@riesd.com>
2014-05-17 15:33:52 -0600

    symbol support for address commands

--- projects/06/assembler/lib/assembler/address.rb
+++ projects/06/assembler/lib/assembler/address.rb
@@ -1,15 +1,31 @@
 module Assembler
   class Address < Command
-    attr_reader :location
+    DIGITS = %w(0 1 2 3 4 5 6 7 8 9 0)
+
+    attr_reader :location, :symbol
 
     def initialize(str)
-      @location = str.slice(1).to_i
-      raise ArgumentError.new("Invalid memory location #{location}") unless location >= 0 && location <= 32767
+      if DIGITS.include?(str[1])
+        @location = str[1..-1].to_i
+        verify_location
+      else
+        @symbol = str[1..-1]
+      end
     end
 
     def binary
       location_binary = location.to_s(2)
       ("0"*(16 - location_binary.size))+location_binary
+    end
+
+    def resolve(table)
+      return true if @location
+      @location = table.fetch(symbol)
+      verify_location
+    end
+
+    def verify_location
+      raise ArgumentError.new("Invalid memory location #{location}") unless location >= 0 && location <= 32767
     end
   end
 end
--- projects/06/assembler/spec/lib/assembler/address_spec.rb
+++ projects/06/assembler/spec/lib/assembler/address_spec.rb
@@ -2,8 +2,18 @@ require 'spec_helper'
 require 'assembler/command'
 
 describe Assembler::Address do
-  Given(:str){ "@1" }
   Given(:address){ Assembler::Address.new(str) }
-  Then{ address.location.should == 1 }
-  Then{ address.binary.should == "0000000000000001" }
+
+  context "constant" do
+    Given(:str){ "@1" }    
+    Then{ address.location.should == 1 }
+    Then{ address.binary.should == "0000000000000001" }
+  end
+
+  context "symbol" do
+    Given(:str){ "@HELLO" }
+    Then{ address.symbol.should == "HELLO" }
+    When{ address.resolve({"HELLO" => 7}) }
+    Then{ address.location.should == 7 }
+  end
 end
