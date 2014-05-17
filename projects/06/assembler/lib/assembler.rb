8039a86700a6660d0f48b6c1720e23a7a7a115dc
Michael Ries <michael@riesd.com>
2014-05-17 14:33:02 -0600

    got the assembler working except for symbols

--- projects/06/assembler/lib/assembler.rb
+++ projects/06/assembler/lib/assembler.rb
@@ -1,5 +1,10 @@
+require 'assembler/parser'
 module Assembler
   def self.assemble(assembly)
-    
+    p = Parser.new(assembly)
+    p.commands.inject("") do |str, command|
+      str << command.binary
+      str << "\n"
+    end
   end
 end
