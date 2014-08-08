61f6cfed53cd764ffa20c65ec889bf0afe73b3c5
Michael Ries <michael@riesd.com>
2014-08-07 23:47:19 -0600

    first tokenize test passing

--- projects/10/jc/lib/jc.ex
+++ projects/10/jc/lib/jc.ex
@@ -1,2 +1,37 @@
 defmodule JackCompiler do
+  def tokenize(jack_str) do
+    tokenize(jack_str, [])
+  end
+
+  defp tokenize(" "<>jack_str, tokens), do: tokenize(jack_str, tokens)
+  defp tokenize("\n"<>jack_str, tokens), do: tokenize(jack_str, tokens)
+  defp tokenize("if"<>jack_str, tokens), do: tokenize(jack_str, [{:keyword, "if"}|tokens])
+  defp tokenize("let"<>jack_str, tokens), do: tokenize(jack_str, [{:keyword, "let"}|tokens])
+  defp tokenize("("<>jack_str, tokens), do: tokenize(jack_str, [{:symbol, "("}|tokens])
+  defp tokenize(")"<>jack_str, tokens), do: tokenize(jack_str, [{:symbol, ")"}|tokens])
+  defp tokenize("{"<>jack_str, tokens), do: tokenize(jack_str, [{:symbol, "{"}|tokens])
+  defp tokenize("}"<>jack_str, tokens), do: tokenize(jack_str, [{:symbol, "}"}|tokens])
+  defp tokenize(">"<>jack_str, tokens), do: tokenize(jack_str, [{:symbol, ">"}|tokens])
+  defp tokenize("<"<>jack_str, tokens), do: tokenize(jack_str, [{:symbol, "<"}|tokens])
+  defp tokenize("="<>jack_str, tokens), do: tokenize(jack_str, [{:symbol, "="}|tokens])
+  defp tokenize(";"<>jack_str, tokens), do: tokenize(jack_str, [{:symbol, ";"}|tokens])
+  defp tokenize("", tokens), do: Enum.reverse(tokens)
+  defp tokenize(jack_str, tokens) do
+    case Regex.named_captures(~r/\A(?<id>[a-zA-Z_][a-zA-Z_\d]*)(?<rest>.*)/s, jack_str) do
+      %{"id" => id, "rest" => rest} ->
+        tokenize(rest, [{:identifier, id}|tokens])
+      nil ->
+        case Regex.named_captures(~r/\A(?<num>\d+)(?<rest>.*)/s, jack_str) do
+          %{"num" => num, "rest" => rest} ->
+            tokenize(rest, [{:integerConstant, num}|tokens])
+          nil ->
+            case Regex.named_captures(~r/\A"(?<s>[^"\n]*)"(?<rest>.*)/s, jack_str) do
+              %{"s" => s, "rest" => rest} ->
+                tokenize(rest, [{:stringConstant,s}|tokens])
+              nil ->
+                tokenize("", tokens)
+            end
+        end
+    end
+  end
 end
--- projects/10/jc/test/jc_test.exs
+++ projects/10/jc/test/jc_test.exs
@@ -1,7 +1,27 @@
 defmodule JackCompilerTest do
   use ExUnit.Case
+  import JackCompiler, only: [tokenize: 1]
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "basic tokenization" do
+    jack = """
+      if (x < 0) 
+        {let state = "negative";}
+    """
+    tokens = [
+      {:keyword, "if"},
+      {:symbol, "("},
+      {:identifier, "x"},
+      {:symbol, "<"},
+      {:integerConstant, "0"},
+      {:symbol, ")"},
+      {:symbol, "{"},
+      {:keyword, "let"},
+      {:identifier, "state"},
+      {:symbol, "="},
+      {:stringConstant, "negative"},
+      {:symbol, ";"},
+      {:symbol, "}"},
+    ]
+    assert tokenize(jack) == tokens
   end
 end
