ab58e1ccca1653b3492afdf68e5eed00251de8af
Michael Ries <michael@riesd.com>
2014-09-10 06:51:52 -0600

    support for arguments and vars in symbol table

--- projects/11/jc/lib/jack/symbol_table.ex
+++ projects/11/jc/lib/jack/symbol_table.ex
@@ -1,6 +1,6 @@
 defmodule Jack.SymbolTable do
   def generate({:class, ast}) do
-    {ast, symbols} = replace(ast, %{"class" => nil, "field" => [], "static" => []})
+    {ast, _symbols} = replace(ast, %{"class" => nil, "field" => [], "static" => [], "argument" => [], "var" => []})
     {:class, ast}
   end
 
@@ -16,6 +16,11 @@ defmodule Jack.SymbolTable do
     {rest,syms} = replace(rest, syms)
     {[{:classVarDec,dec}|rest],syms}
   end
+  defp replace([{:subroutineDec,dec}|rest], syms) do
+    {dec, syms} = sub(dec,syms)
+    {rest,syms} = replace(rest, syms)
+    {[{:subroutineDec,dec}|rest],syms}
+  end
   defp replace([],symbols) do
     {[], symbols}
   end
@@ -25,10 +30,10 @@ defmodule Jack.SymbolTable do
   end
 
   defp cvd([], syms, _temp), do: {[], syms}
-  defp cvd([{:keyword, kw}|tail], syms, temp) when (kw in ["field","static"]) do
+  defp cvd([{:keyword, kw},type|tail], syms, temp) when (kw in ["field","static"]) do
     temp = Dict.put(temp, :category, kw)
     {tail, syms} = cvd(tail, syms, temp)
-    {[{:keyword, kw}|tail],syms}
+    {[{:keyword, kw},type|tail],syms}
   end
   defp cvd([{:identifier,id}|tail], syms, %{:category => c}=temp) do
     idx = (syms |> Dict.get(c) |> Enum.count) + 1
@@ -39,6 +44,72 @@ defmodule Jack.SymbolTable do
   end
   defp cvd([head|tail], syms, temp) do
     {tail, syms} = cvd(tail, syms, temp)
+    {[head|tail], syms}
+  end
+
+  defp sub([], syms), do: {[], syms}
+  defp sub([{:keyword,kw},type,{:identifier,id}|tail], syms) when (kw in ["function","method"]) do
+    id_map = %{:name => id, :category => "subroutine", :definition => true}
+    {tail,syms} = sub(tail, syms)
+    {[{:keyword,kw},type,{:identifier, id_map}|tail], syms}
+  end
+  defp sub([{:parameterList,params}|tail], syms) do
+    {params, syms} = params(params,syms)
+    {tail,syms} = sub(tail, syms)
+    {[{:parameterList,params}|tail], syms}
+  end
+  defp sub([{:subroutineBody,body}|tail], syms) do
+    {body, syms} = body(body,syms)
+    {tail,syms} = sub(tail,syms)
+    {[{:subroutineBody,body}|tail], syms}
+  end
+  defp sub([head|tail], syms) do
+    {tail, syms} = sub(tail, syms)
+    {[head|tail], syms}
+  end
+
+  defp params([],syms), do: {[],syms}
+  defp params([{:keyword, "var"}|tail],syms) do
+    {tail, syms} = params(tail, syms)
+    {[{:keyword,"var"}|tail], syms}
+  end
+  defp params([type,{:identifier, id}|tail],syms) do
+    idx = (syms |> Dict.get("argument") |> Enum.count) + 1
+    id_map = %{:name => id, :category => "argument", :index => idx, :definition => true}
+    syms = Dict.update!(syms, "argument", &([id_map|&1]))
+    {tail, syms} = params(tail, syms)
+    {[type,{:identifier,id_map}|tail], syms}
+  end
+  defp params([head|tail], syms) do
+    {tail, syms} = params(tail, syms)
+    {[head|tail], syms}
+  end
+
+  defp body([],syms), do: {[],syms}
+  defp body([{:varDec,dec}|tail], syms) do
+    {dec, syms} = vd(dec, syms)
+    {tail, syms} = body(tail, syms)
+    {[{:varDec,dec}|tail],syms}
+  end
+  defp body([head|tail], syms) do
+    {tail,syms} = body(tail, syms)
+    {[head|tail], syms}
+  end
+
+  defp vd([], syms), do: {[], syms}
+  defp vd([{:keyword, "var"},type|tail], syms) do
+    {tail, syms} = vd(tail, syms)
+    {[{:keyword, "var"},type|tail],syms}
+  end
+  defp vd([{:identifier,id}|tail], syms) do
+    idx = (syms |> Dict.get("var") |> Enum.count) + 1
+    id_map = %{ :category => "var", :name => id, :definition => true, :index => idx }
+    syms = Dict.update!(syms, "var", &([id_map|&1]))
+    {tail, syms} = vd(tail, syms)
+    {[{:identifier, id_map}|tail], syms}
+  end
+  defp vd([head|tail], syms) do
+    {tail, syms} = vd(tail, syms)
     {[head|tail], syms}
   end
 end
--- projects/11/jc/test/symbol_table_test.exs
+++ projects/11/jc/test/symbol_table_test.exs
@@ -4,11 +4,11 @@ defmodule SymbolTableTest do
   import Jack.Parser, only: [parse: 1]
   import Jack.SymbolTable, only: [generate: 1]
 
-  test "simple case" do
+  test "class fields and statics" do
     jack = """
       class Simple {
         field int x, y;
-        static int debug;
+        static String debug;
       }
     """
 
@@ -37,13 +37,62 @@ defmodule SymbolTableTest do
       ],
       classVarDec: [
         keyword: "static",
-        keyword: "int",
+        identifier: "String",
         identifier: %{
           :name => "debug",
           :category => "static",
           :index => 1,
           :definition => true },
         symbol: ";"
+      ],
+      symbol: "}"
+    ]}
+
+    assert jack |> tokenize |> parse |> generate == expected
+  end
+
+  test "argument and var declarations" do
+    jack = """
+      class Simple {
+        function void main(int argc, int argv){
+          var String s;
+          var int x, y;
+        }
+      }
+    """
+
+    expected = {:class, [
+      keyword: "class",
+      identifier: %{ :name => "Simple", :category => "class", :definition => true },
+      symbol: "{",
+      subroutineDec: [
+        keyword: "function",
+        keyword: "void",
+        identifier: %{ :name => "main", :category => "subroutine", :definition => true},
+        symbol: "(",
+        parameterList: [
+          keyword: "int",
+          identifier: %{:name => "argc", :category => "argument", :definition => true, :index => 1},
+          symbol: ",",
+          keyword: "int",
+          identifier: %{:name => "argv", :category => "argument", :definition => true, :index => 2} ],
+        symbol: ")",
+        subroutineBody: [
+          symbol: "{",
+          varDec: [
+            keyword: "var",
+            identifier: "String",
+            identifier: %{:name => "s", :category => "var", :definition => true, :index => 1},
+            symbol: ";" ],
+          varDec: [
+            keyword: "var",
+            keyword: "int",
+            identifier: %{:name => "x", :category => "var", :definition => true, :index => 2},
+            symbol: ",",
+            identifier: %{:name => "y", :category => "var", :definition => true, :index => 3},
+            symbol: ";"],
+          statements: [],
+          symbol: "}" ]
       ],
       symbol: "}"
     ]}
