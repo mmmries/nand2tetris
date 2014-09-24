9d968fa9f19b8f4fa8180aa36a9dc14279d9b758
Michael Ries <michael@riesd.com>
2014-09-23 23:14:56 -0600

    use a map of meta data instead of just a single index

--- projects/11/jc/lib/jack/if_collapser.ex
+++ projects/11/jc/lib/jack/if_collapser.ex
@@ -1,33 +1,33 @@
 defmodule Jack.IfCollapser do
   def collapse(ast) do
-    {ast, idx} = collapse(ast, 0)
+    {ast, _counts} = collapse(ast, %{ifs: 0})
     ast
   end
 
-  def collapse([{:ifStatement, statement}|tail], idx) do
+  def collapse([{:ifStatement, statement}|tail], %{ifs: count}=counts) do
     {condition, statement} = condition(statement)
     {true_statements, statement} = statements(statement)
-    {true_statements, sub_idx} = collapse(true_statements, idx + 1)
+    {true_statements, counts} = collapse(true_statements, %{counts | ifs: count+1})
     {else_or_empty, _statement} = else_or_empty(statement)
-    {else_or_empty, sub_idx} = collapse(else_or_empty, sub_idx)
+    {else_or_empty, counts} = collapse(else_or_empty, counts)
     map = %{
-      index: idx,
+      index: count,
       condition: condition,
       true_statements: true_statements,
       false_statements: else_or_empty,
     }
-    {tail,sub_idx} = collapse(tail,sub_idx)
-    {[{:ifStatement, map}|tail], sub_idx}
+    {tail,counts} = collapse(tail,counts)
+    {[{:ifStatement, map}|tail], counts}
   end
-  def collapse([],idx), do: {[], idx}
-  def collapse([{type,list}|tail],idx) when is_list(list) do
-    {list, idx} = collapse(list,idx)
-    {tail, idx} = collapse(tail, idx)
-    {[{type,list}|tail], idx}
+  def collapse([],counts), do: {[], counts}
+  def collapse([{type,list}|tail],counts) when is_list(list) do
+    {list, counts} = collapse(list, counts)
+    {tail, counts} = collapse(tail, counts)
+    {[{type,list}|tail], counts}
   end
-  def collapse([head|tail],idx) do
-    {tail, idx} = collapse(tail,idx)
-    {[head|tail], idx}
+  def collapse([head|tail],counts) do
+    {tail, counts} = collapse(tail,counts)
+    {[head|tail], counts}
   end
 
   defp condition([{:keyword,"if"},{:symbol,"("},{:expression,exp},{:symbol,")"}| tail]) do
